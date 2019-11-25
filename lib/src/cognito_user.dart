113eef55afa9a12d86df088c1b02ccd95ef064e6
graham <graham@codeheroes.com.au>
2019-11-25 12:05:01 +1000

    revert/fix for issue after user completes FORCE_CHANGE_PASSWORD, being presented with a UserConfirmationNecessary error

--- lib/src/cognito_user.dart
+++ lib/src/cognito_user.dart
@@ -142,7 +142,7 @@ class CognitoUser {
         ['DeviceKey'];
     await cacheDeviceKeyAndPassword();
 
-    if (dataConfirm['UserConfirmationNecessary'] == true) {
+    if (dataConfirm['UserConfirmationNecessary'] == true && !_signInUserSession.isValid()) {
       throw new CognitoUserConfirmationNecessaryException(
           signInUserSession: _signInUserSession);
     }
@@ -785,7 +785,7 @@ class CognitoUser {
     _deviceKey = dataAuthenticate['AuthenticationResult']['NewDeviceMetadata']
         ['DeviceKey'];
     await cacheDeviceKeyAndPassword();
-    if (dataConfirm['UserConfirmationNecessary'] == true && !_signInUserSession.isValid()) {
+    if (dataConfirm['UserConfirmationNecessary'] == true) {
       throw new CognitoUserConfirmationNecessaryException(
           signInUserSession: _signInUserSession);
     }
